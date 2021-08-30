3080e775ee21409c6ed5849c6e162c0f6115b1f4
Rushikesh <pitalerushikesh@gmail.com>
2021-08-31 03:10:22 +0530

    added preloader with more features

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-import Preloader from "./Pages/components/Preloader";
 
 import Home from "./Pages/Home";
 
 const Routes = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  return loading ? (
-    <Preloader />
-  ) : (
+  return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
 import "animate.css";
-import "./App.css";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import Preloader from "./Pages/components/Preloader";
+
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    // this simulates an async action, after which the component will render the content
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      // if your component doesn't have to wait for async data, remove this block
+      return <Preloader />; // render null when app is not ready
+    }
+
+    return <Routes />;
+  }
+}
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
